c8e20da8b3dd7875c929df231e6c51d5a85202b8
Dhruvit Kachhiya <dhruvit.kachhiya@visilean.com>
2023-09-12 13:18:19 +0530

    ToggleVC - added ToggleView via "addSubView" method

--- DesignComponentsDemo/DesignComponentsDemo/ToggleVC.swift
+++ DesignComponentsDemo/DesignComponentsDemo/ToggleVC.swift
@@ -11,6 +11,7 @@ import DesignComponents
 class ToggleVC: UIViewController {
     
     @IBOutlet weak var toggleView: ToggleView!
+    var toggleView2: ToggleView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,13 @@ class ToggleVC: UIViewController {
         toggleView.addTarget(self, action: #selector(toggleValueChanged(_:)), for: .valueChanged)
         toggleView.setTitle("Remember me")
         toggleView.setDescription("Save my login details for next time.")
+        
+        toggleView2 = ToggleView()
+        toggleView2.frame = CGRect(x: 32, y: 100, width: self.view.frame.size.width - 64, height: 50)
+        toggleView2.addTarget(self, action: #selector(toggleValueChanged(_:)), for: .valueChanged)
+        toggleView2.setTitle("Remember me")
+        toggleView2.setDescription("Save my login details for next time.")
+        self.view.addSubview(toggleView2)
     }
     
     @objc func toggleValueChanged(_ sender: ToggleView) {
