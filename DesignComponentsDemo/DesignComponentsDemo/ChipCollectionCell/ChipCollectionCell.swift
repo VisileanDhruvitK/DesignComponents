b5399979f8c3769c0f012e24af29e63ce27553ec
Dhruvit Kachhiya <dhruvit.kachhiya@visilean.com>
2023-12-27 12:50:41 +0530

    ChipCollectionCell - height constraint related changes

--- DesignComponentsDemo/DesignComponentsDemo/ChipCollectionCell/ChipCollectionCell.swift
+++ DesignComponentsDemo/DesignComponentsDemo/ChipCollectionCell/ChipCollectionCell.swift
@@ -11,6 +11,7 @@ import DesignComponents
 class ChipCollectionCell: UICollectionViewCell {
     
     @IBOutlet weak var chipControl: ChipControl!
+    @IBOutlet weak var chipControlHeight: NSLayoutConstraint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,16 +19,24 @@ class ChipCollectionCell: UICollectionViewCell {
     }
     
     func configureCell(title: String, isSelected: Bool, size: ComponentSize = .medium, chipStyle: ChipStyle = .roundPA) {
-        let option = ChipOption(title: title, isSelected: isSelected, chipType: .textOnly, chipStyle: chipStyle)
         chipControl.componentSize = size
+        
+        let option = ChipOption(title: title, isSelected: isSelected, chipType: .textOnly, chipStyle: chipStyle)
         chipControl.setOption(option: option)
         chipControl.isUserInteractionEnabled = false
+        setHeight(height: (size == .medium ? 40 : 32))
     }
     
     func configureCell(option: ChipOption, size: ComponentSize = .medium) {
-        chipControl.setOption(option: option)
         chipControl.componentSize = size
+        chipControl.setOption(option: option)
         chipControl.isUserInteractionEnabled = false
+        setHeight(height: (size == .medium ? 40 : 32))
+    }
+    
+    func setHeight(height: CGFloat) {
+        chipControlHeight.constant = height
+        self.chipControl.layoutIfNeeded()
     }
     
 }
